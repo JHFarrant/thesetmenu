639aacf0da96a13677d03e2c6feafd58fd4cd372
Ryan Burke <ryan.burke@babylonhealth.com>
2024-06-04 09:36:21 +0100

    make footer year dynamic

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -1,19 +1,24 @@
+import React from "react";
+
 import { Footer } from "flowbite-react";
 
-const PageFooter: React.FC = () => (
-  <Footer container>
-    <div className="w-full flex justify-between items-center p-4">
-      <Footer.Copyright
-        by="My Glasto Set Menu"
-        href="/"
-        year={2024}
-      />
-      <Footer.LinkGroup>
-        <Footer.Link href="/disclaimer">Disclaimer</Footer.Link>
-        <Footer.Link href="mailto:jhfarrant@gmail.com">Contact</Footer.Link>
-      </Footer.LinkGroup>
-    </div>
-  </Footer>
-);
+const PageFooter: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <Footer container>
+      <div className="w-full flex justify-between items-center p-4">
+        <Footer.Copyright
+          by="My Glasto Set Menu"
+          href="/"
+          year={currentYear}
+        />
+        <Footer.LinkGroup>
+          <Footer.Link href="/disclaimer">Disclaimer</Footer.Link>
+          <Footer.Link href="mailto:jhfarrant@gmail.com">Contact</Footer.Link>
+        </Footer.LinkGroup>
+      </div>
+    </Footer>
+  );
+};
 
 export default PageFooter;
